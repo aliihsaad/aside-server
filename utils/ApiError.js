527121a969b470d560261298360b9b70d8f7752f
Ali Saad <59366979+aliihsaad@users.noreply.github.com>
2026-08-04 01:57:05 +0200

    added ApiError class with status code factories

---
+++ utils/ApiError.js
@@ -0,0 +1,15 @@
+export default class ApiError extends Error {
+    constructor(status, message) {
+        super(message);
+        this.status = status;
+        this.isApiError = true;
+        // trace starts at the line that actually threw instead of at this file.
+        Error.captureStackTrace(this, this.constructor);
+    }
+
+    static badRequest(msg ="Bad request") {return new ApiError(400, msg); }
+    static unauthorized(msg = "Unauthorized") { return new ApiError(401, msg); }
+    static forbidden(msg = "You can't do that") { return new ApiError(403, msg); }
+    static notFound(msg = "Not found") {return new ApiError(404, msg); }
+    static conflict(msg = "Already exists") { return new ApiError(409, msg); }
+}
