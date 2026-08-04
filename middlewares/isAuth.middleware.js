2b196a5f9fc06dc00f6d254f946765495301b79a
Ali Saad <59366979+aliihsaad@users.noreply.github.com>
2026-08-04 11:52:31 +0200

    added isAuth middleware for bearer token verification

---
+++ middlewares/isAuth.middleware.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import ApiError from "../utils/ApiError.js";
+
+export default function isAuth(req, res, next) {
+    const header = req.headers.authorization;
+    if (!header || !header.startsWith("Bearer ")) {
+        return next(ApirError.unauthorized("No token provided"));
+    }
+
+    try {
+        req.user = jwt.verify(header.split(" ")[1], process.env.TOKEN_SECRET);
+        next();
+    } catch {
+        next(ApiError.unauthorized("Token is invalid or expired"));
+    }
+}
